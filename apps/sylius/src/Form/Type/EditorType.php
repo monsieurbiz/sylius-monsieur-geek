025e0ca94ba2842b1480b03b5cd5afdab1e182ae
Maxime Huran <m.huran@monsieurbiz.com>
2019-05-14 11:04:48 +0200

    Use Editor Form Type for editor entity

---
+++ apps/sylius/src/Form/Type/EditorType.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class EditorType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->addEventSubscriber(new AddCodeFormSubscriber())
+        ;
+    }
+}
