f0493d4244eefdd0621823326738016247990993
Maxime Huran <m.huran@monsieurbiz.com>
2019-05-14 11:42:43 +0200

    Add editor to product entity in many to one relation

--- apps/sylius/src/Entity/Product/Product.php
+++ apps/sylius/src/Entity/Product/Product.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use App\Entity\Editor\EditorInterface;
 use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
 use Doctrine\ORM\Mapping\Table;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 use Sylius\Component\Product\Model\ProductTranslationInterface;
@@ -15,8 +18,32 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
  */
 class Product extends BaseProduct
 {
+    /** @var EditorInterface|null
+     *
+     * @ManyToOne(targetEntity="App\Entity\Editor\EditorInterface")
+     * @JoinColumn(name="app_editor_id", referencedColumnName="id", nullable=true)
+     */
+    private $editor;
+    
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
     }
+    
+    /**
+     * @return EditorInterface|null
+     */
+    public function getEditor(): ?EditorInterface
+    {
+        return $this->editor;
+    }
+    
+    /**
+     * @param EditorInterface|null $editor
+     */
+    public function setEditor(?EditorInterface $editor): void
+    {
+        $this->editor = $editor;
+    }
+    
 }
