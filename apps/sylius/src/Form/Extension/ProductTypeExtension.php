95dd9ba26a7c4271cfd041df9be98e77efc9d0ca
Maxime Huran <m.huran@monsieurbiz.com>
2019-05-14 13:55:37 +0200

    Add product type form extension

---
+++ apps/sylius/src/Form/Extension/ProductTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form\Extension;
+
+use App\Entity\Editor\Editor;
+use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
+use Symfony\Component\Form\FormTypeExtensionInterface;
+
+/**
+ * @method iterable getExtendedTypes()
+ */
+class ProductTypeExtension extends AbstractResourceType implements FormTypeExtensionInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('editor', EntityType::class, [
+            'class' => Editor::class,
+            'choice_label' => 'name',
+            'choice_value' => 'code',
+        ]);
+    }
+    
+    public function getExtendedType(): string
+    {
+        return ProductType::class;
+    }
+}
